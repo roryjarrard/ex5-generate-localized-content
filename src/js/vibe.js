3c6a14a2f310333df6fb0fe5f3a235009a302eed
Rory Jarrard <rjarrard@frontsteps.com>
2021-06-04 11:59:41 -0600

    Initial commit

---
+++ src/js/vibe.js
@@ -0,0 +1,11 @@
+const vibes = [
+  '... and you are awesome!',
+  '... have a wonderful day!',
+  "... and you've got this!",
+  '... and so is this puppy! 🐶',
+]
+
+// choose a random good vibe
+var vibe = vibes[Math.floor(Math.random() * Math.floor(vibes.length))]
+
+document.querySelector('.vibe').append(vibe)

771d213ecfa9f30a872f8bfb67c861b545d250a8
Rory Jarrard <rjarrard@frontsteps.com>
2021-06-04 15:24:35 -0600

    call and return data for each country

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,25 @@
 const axios = require('axios')
+const countries = require('./countries.json')
 require('dotenv').config()
 
-module.exports = async function () {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=Apple&from=2021-06-04&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     )
-    return response.data
+    return {
+      country: country,
+      articles: response.data.articles,
+    }
   } catch (error) {
     console.log({ error })
   }
+}
+
+module.exports = async function () {
+  let newsPromises = countries.map(getNews)
+  return Promise.all(newsPromises).then((newsObjects) => {
+    console.log('newsObjects', newsObjects)
+    return [].concat.apply([], newsObjects)
+  })
 }

3f9065ffc493284a1d3f430b87f9d9ca2f258804
Rory Jarrard <rjarrard@frontsteps.com>
2021-06-04 14:00:58 -0600

    update key

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 module.exports = async function () {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=Apple&from=2021-06-04&sortBy=popularity&apiKey=${process.env.API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/everything?q=Apple&from=2021-06-04&sortBy=popularity&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     )
     console.log(response.data)
     return response.data

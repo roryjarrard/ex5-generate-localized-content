06b2c25c53a96e09554379f5f3c7ad207ec65d2e
Rory Jarrard <rjarrard@frontsteps.com>
2021-06-04 13:53:22 -0600

    fetch articles from newsapi

---
+++ src/_data/news.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+require('dotenv').config()
+
+module.exports = async function () {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=Apple&from=2021-06-04&sortBy=popularity&apiKey=${process.env.API_KEY}&pageSize=5`
+    )
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.log({ error })
+  }
+}

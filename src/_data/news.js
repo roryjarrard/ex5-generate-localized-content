49545bacc42c0e3671a790ee464c99b5d9f7348c
Rory Jarrard <rjarrard@frontsteps.com>
2021-06-08 08:41:38 -0600

    clean up

--- src/_data/news.js
+++ src/_data/news.js
@@ -7,12 +7,13 @@ async function getNews(country) {
     const response = await axios.get(
       `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     )
+
     return {
-      country: country,
-      articles: response.data.articles,
+      "country": country,
+      "articles": response.data.articles,
     }
   } catch (error) {
-    console.log({ error })
+    console.log({error})
   }
 }
 
